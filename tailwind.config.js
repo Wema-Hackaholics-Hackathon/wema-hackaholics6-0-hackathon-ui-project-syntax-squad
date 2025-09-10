e5b74436cbdf390a75a58e4da94a12a2d3fbde26
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-09-10 13:28:21 +0000

    style: integrate Tailwind (config, deps, directives)

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
+  content: [
+    './src/**/*.{ts,tsx,js,jsx}',
+    './alatlens/**/*.{ts,tsx,js,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
